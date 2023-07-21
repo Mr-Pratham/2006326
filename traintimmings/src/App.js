825916f7d815b2b9305794db606751b6af88c1c1
pratham <prathamphenomenol@gmail.com>
2023-07-21 15:29:16 +0530

    first commit

---
+++ traintimmings/src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Table from "./components/Table";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Table/>
+      <Footer />
+    </div>
+  );
+}
